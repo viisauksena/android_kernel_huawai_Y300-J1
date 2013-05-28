1e20ed28b3a007ba387e03161f05e16c3175d54c
Saket Saurabh <ssaurabh@codeaurora.org>
2013-05-28 14:43:58 +0530

    msm_serial_hs: Add check to validate wakeup irq in UART clock on request
    Wakeup irq is UART RX GPIO IRQ line to be configured as wakeup source and
    is used as an optional property with UART. By default wakeup irq is set as
    zero when it is not to be used with UART.
    During UART clock on request msm_hs_request_clock_on(), calling
    disable_irq_nosync() without validating wakeup irq is causing warning
    message.
    Hence fixing the issue by adding check to validate the wakeup irq during
    UART clock on request.
    
    CRs-Fixed: 490597
    Change-Id: Iff014f41428d590117e469c1b470736383a78848
    Signed-off-by: Saket Saurabh <ssaurabh@codeaurora.org>

--- drivers/tty/serial/msm_serial_hs.c
+++ drivers/tty/serial/msm_serial_hs.c
@@ -1555,7 +1555,8 @@ void msm_hs_request_clock_on(struct uart_port *uport)
 	switch (msm_uport->clk_state) {
 	case MSM_HS_CLK_OFF:
 		wake_lock(&msm_uport->dma_wake_lock);
-		disable_irq_nosync(msm_uport->wakeup.irq);
+		if (use_low_power_wakeup(msm_uport))
+			disable_irq_nosync(msm_uport->wakeup.irq);
 		spin_unlock_irqrestore(&uport->lock, flags);
 		ret = clk_prepare_enable(msm_uport->clk);
 		if (ret) {
@@ -1784,7 +1785,8 @@ static int msm_hs_startup(struct uart_port *uport)
 free_uart_irq:
 	free_irq(uport->irq, msm_uport);
 free_wake_irq:
-	irq_set_irq_wake(msm_uport->wakeup.irq, 0);
+	if (use_low_power_wakeup(msm_uport))
+		irq_set_irq_wake(msm_uport->wakeup.irq, 0);
 deinit_uart_clk:
 	clk_disable_unprepare(msm_uport->clk);
 	if (msm_uport->pclk)
